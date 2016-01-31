aad813159ba8b7546464bc12a322aabc31a4d03f
masterxeon1001 <masterxeon1001@gmail.com>
2016-01-31 05:32:22 -0600

    file update
    push

---
+++ panel.py
@@ -0,0 +1,28 @@
+import bpy
+
+class n5(bpy.types.Panel):
+    bl_idname = "n"
+    bl_label = "N"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "category"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.label("text")
+        layout.menu("view3d.nmenu", text = "N5 Menu")
+        row.template_icon_view(WM, "nexus_five_previews")
+        
+class N5Menu(bpy.types.Menu):
+    bl_idname = "view3d.nmenu"
+    bl_label = "Nmenu"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.label("text")
+        
+
+        
+                
